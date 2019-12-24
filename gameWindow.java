17556878dae9a7463708017a915de00ca9f04017
Callum <57758785+CallumCC@users.noreply.github.com>
2019-12-24 14:33:13 +0200

    2 Player done

--- gameWindow.java
+++ gameWindow.java
@@ -2,9 +2,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class gameWindow {
 
@@ -17,6 +19,7 @@ public class gameWindow {
     @FXML private Canvas seven;
     @FXML private Canvas eight;
     @FXML private Canvas nine;
+    @FXML private Label winLabel;
     
     private Image x = new Image("X.png", 100, 100, false, false);
     private Image o = new Image("O.png", 100, 100, false, false);
@@ -44,6 +47,16 @@ public class gameWindow {
     GraphicsContext gc7;
     GraphicsContext gc8;
     GraphicsContext gc9;
+    
+    private String oneXO = null;
+    private String twoXO = null;
+    private String threeXO = null;
+    private String fourXO = null;
+    private String fiveXO = null;
+    private String sixXO = null;
+    private String sevenXO = null;
+    private String eightXO = null;
+    private String nineXO = null;
 
     @FXML 
     void oneSelect(MouseEvent event) {
@@ -53,13 +66,15 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc1.drawImage(x, 15, 15);
+        		oneXO = "x";
         	} else {
         		gc1.drawImage(o, 15, 15);
+        		oneXO = "o";
         	}
+    		clickOne = true;
+        	checkWin();
     	}
-    	
-    	clickOne = true;
-    	checkFinish();
+
     }
 
     @FXML
@@ -70,14 +85,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc2.drawImage(x, 15, 15);
+        		twoXO = "x";
         	} else {
         		gc2.drawImage(o, 15, 15);
+        		twoXO = "o";
         	}
+    		clickTwo = true;
+        	checkWin();
     	}
-    	
-    	clickTwo = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -88,14 +103,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc3.drawImage(x, 15, 15);
+        		threeXO = "x";
         	} else {
         		gc3.drawImage(o, 15, 15);
+        		threeXO = "o";
         	}
+    		clickThree = true;
+        	checkWin();
     	}
-    	
-    	clickThree = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -106,14 +121,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc4.drawImage(x, 15, 15);
+        		fourXO = "x";
         	} else {
         		gc4.drawImage(o, 15, 15);
+        		fourXO = "o";
         	}
+    		clickFour = true;
+        	checkWin();
     	}
-    	
-    	clickFour = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -124,14 +139,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc5.drawImage(x, 15, 15);
+        		fiveXO = "x";
         	} else {
         		gc5.drawImage(o, 15, 15);
+        		fiveXO = "o";
         	}
+    		clickFive = true;
+        	checkWin();
     	}
-    	
-    	clickFive = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -142,14 +157,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc6.drawImage(x, 15, 15);
+        		sixXO = "x";
         	} else {
         		gc6.drawImage(o, 15, 15);
+        		sixXO = "o";
         	}
+    		clickSix = true;
+        	checkWin();
     	}
-    	
-    	clickSix = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -160,14 +175,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc7.drawImage(x, 15, 15);
+        		sevenXO = "x";
         	} else {
         		gc7.drawImage(o, 15, 15);
+        		sevenXO = "o";
         	}
+    		clickSeven = true;
+        	checkWin();
     	}
-    	
-    	clickSeven = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -177,14 +192,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc8.drawImage(x, 15, 15);
+        		eightXO = "x";
         	} else {
         		gc8.drawImage(o, 15, 15);
+        		eightXO = "o";
         	}
+    		clickEight = true;
+        	checkWin();
     	}
-    	
-    	clickEight = true;
-    	checkFinish();
-
     }
 
     @FXML
@@ -194,14 +209,14 @@ public class gameWindow {
         	takeTurn();
     		if(XorO == true) {
         		gc9.drawImage(x, 15, 15);
+        		nineXO = "x";
         	} else {
         		gc9.drawImage(o, 15, 15);
+        		nineXO = "o";
         	}
+    		clickNine = true;
+        	checkWin();
     	}
-    	
-    	clickNine = true;
-    	checkFinish();
-
     }
     
     @FXML
@@ -236,6 +251,8 @@ public class gameWindow {
     		gc9.clearRect(0, 0, 133, 133);
     	}
     	
+    	winLabel.setText("");
+    	
     	clickOne = false;
         clickTwo = false;
         clickThree = false;
@@ -247,17 +264,19 @@ public class gameWindow {
         clickNine = false;
         
         turn = 0;
+        
+        oneXO = null;
+        twoXO = null;
+        threeXO = null;
+        fourXO = null;
+        fiveXO = null;
+        sixXO = null;
+        sevenXO = null;
+        eightXO = null;
+        nineXO = null;
+        
     }
-    
-    public void checkFinish() {
-    	if(clickOne == true && clickTwo == true && clickThree == true && clickFour == true 
-    			&& clickFive == true && clickSix == true && clickSeven == true && clickEight == true
-    			&& clickNine == true) {
-    		
-    		System.out.println("Finished");
-    	}
-    }
-    
+
     public void takeTurn() {
     	if(turn % 2 == 0) {
     		XorO = true;
@@ -265,6 +284,237 @@ public class gameWindow {
     		XorO = false;
     	}
     	turn++;
+    }
+    
+    public void checkWin() {
+    	if(clickOne && clickTwo && clickThree) {
+    		if(oneXO.equals("x") && twoXO.equals("x") && threeXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc1.setFill(Color.DEEPPINK);
+    			gc1.fillRect(0, 58, 133, 3);
+    			gc2.setFill(Color.DEEPPINK);
+    			gc2.fillRect(0, 58, 133, 3);
+    			gc3.setFill(Color.DEEPPINK);
+    			gc3.fillRect(0, 58, 133, 3);
+    			win("X");
+    		} else if(oneXO.equals("o") && twoXO.equals("o") && threeXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc1.setFill(Color.DEEPPINK);
+    			gc1.fillRect(0, 58, 133, 3);
+    			gc2.setFill(Color.DEEPPINK);
+    			gc2.fillRect(0, 58, 133, 3);
+    			gc3.setFill(Color.DEEPPINK);
+    			gc3.fillRect(0, 58, 133, 3);
+    			win("O");
+    		} 
+    	}
+    	if(clickFour && clickFive && clickSix) {
+    		if(fourXO.equals("x") && fiveXO.equals("x") && sixXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc4.setFill(Color.DEEPPINK);
+    			gc4.fillRect(0, 58, 133, 3);
+    			gc5.setFill(Color.DEEPPINK);
+    			gc5.fillRect(0, 58, 133, 3);
+    			gc6.setFill(Color.DEEPPINK);
+    			gc6.fillRect(0, 58, 133, 3);
+    			win("X");
+    		} else if(fourXO.equals("o") && fiveXO.equals("o") && sixXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc4.setFill(Color.DEEPPINK);
+    			gc4.fillRect(0, 58, 133, 3);
+    			gc5.setFill(Color.DEEPPINK);
+    			gc5.fillRect(0, 58, 133, 3);
+    			gc6.setFill(Color.DEEPPINK);
+    			gc6.fillRect(0, 58, 133, 3);
+    			win("O");
+    		} 
+    	}
+    	if(clickSeven && clickEight && clickNine) {
+    		if(sevenXO.equals("x") && eightXO.equals("x") && nineXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc7.setFill(Color.DEEPPINK);
+    			gc7.fillRect(0, 58, 133, 3);
+    			gc8.setFill(Color.DEEPPINK);
+    			gc8.fillRect(0, 58, 133, 3);
+    			gc9.setFill(Color.DEEPPINK);
+    			gc9.fillRect(0, 58, 133, 3);
+    			win("X");
+    		} else if(sevenXO.equals("o") && eightXO.equals("o") && nineXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc7.setFill(Color.DEEPPINK);
+    			gc7.fillRect(0, 58, 133, 3);
+    			gc8.setFill(Color.DEEPPINK);
+    			gc8.fillRect(0, 58, 133, 3);
+    			gc9.setFill(Color.DEEPPINK);
+    			gc9.fillRect(0, 58, 133, 3);
+    			win("O");
+    		}
+    	}
+    	if(clickOne && clickFour && clickSeven) {
+    		if(oneXO.equals("x") && fourXO.equals("x") && sevenXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc1.setFill(Color.DEEPPINK);
+    			gc1.fillRect(67, 0, 3, 133);
+    			gc4.setFill(Color.DEEPPINK);
+    			gc4.fillRect(67, 0, 3, 133);
+    			gc7.setFill(Color.DEEPPINK);
+    			gc7.fillRect(67, 0, 3, 133);
+    			win("X");
+    		} else if(oneXO.equals("o") && fourXO.equals("o") && sevenXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc1.setFill(Color.DEEPPINK);
+    			gc1.fillRect(67, 0, 3, 133);
+    			gc4.setFill(Color.DEEPPINK);
+    			gc4.fillRect(67, 0, 3, 133);
+    			gc7.setFill(Color.DEEPPINK);
+    			gc7.fillRect(67, 0, 3, 133);
+    			win("O");
+    		} 
+    	}
+    	if(clickTwo && clickFive && clickEight) {
+    		if(twoXO.equals("x") && fiveXO.equals("x") && eightXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc2.setFill(Color.DEEPPINK);
+    			gc2.fillRect(67, 0, 3, 133);
+    			gc5.setFill(Color.DEEPPINK);
+    			gc5.fillRect(67, 0, 3, 133);
+    			gc8.setFill(Color.DEEPPINK);
+    			gc8.fillRect(67, 0, 3, 133);
+    			win("X");
+    		} else if(twoXO.equals("o") && fiveXO.equals("o") && eightXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc2.setFill(Color.DEEPPINK);
+    			gc2.fillRect(67, 0, 3, 133);
+    			gc5.setFill(Color.DEEPPINK);
+    			gc5.fillRect(67, 0, 3, 133);
+    			gc8.setFill(Color.DEEPPINK);
+    			gc8.fillRect(67, 0, 3, 133);
+    			win("O");
+    		} 
+    	}
+    	if(clickThree && clickSix && clickNine) {
+    		if(threeXO.equals("x") && sixXO.equals("x") && nineXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc3.setFill(Color.DEEPPINK);
+    			gc3.fillRect(67, 0, 3, 133);
+    			gc6.setFill(Color.DEEPPINK);
+    			gc6.fillRect(67, 0, 3, 133);
+    			gc9.setFill(Color.DEEPPINK);
+    			gc9.fillRect(67, 0, 3, 133);
+    			win("X");
+    		} else if(threeXO.equals("o") && sixXO.equals("o") && nineXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc3.setFill(Color.DEEPPINK);
+    			gc3.fillRect(67, 0, 3, 133);
+    			gc6.setFill(Color.DEEPPINK);
+    			gc6.fillRect(67, 0, 3, 133);
+    			gc9.setFill(Color.DEEPPINK);
+    			gc9.fillRect(67, 0, 3, 133);
+    			win("O");
+    		} 
+    	}
+    	if(clickOne && clickFive && clickNine) {
+    		if(oneXO.equals("x") && fiveXO.equals("x") && nineXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc1.setStroke(Color.DEEPPINK);
+    			gc1.setLineWidth(4);
+    			gc1.strokeLine(0, 0, 133, 133);
+    			gc5.setStroke(Color.DEEPPINK);
+    			gc5.setLineWidth(4);
+    			gc5.strokeLine(0, 0, 133, 133);
+    			gc9.setStroke(Color.DEEPPINK);
+    			gc9.setLineWidth(4);
+    			gc9.strokeLine(0, 0, 133, 133);
+    			win("X");
+    		} else if(oneXO.equals("o") && fiveXO.equals("o") && nineXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc1.setStroke(Color.DEEPPINK);
+    			gc1.setLineWidth(4);
+    			gc1.strokeLine(0, 0, 133, 133);
+    			gc5.setStroke(Color.DEEPPINK);
+    			gc5.setLineWidth(4);
+    			gc5.strokeLine(0, 0, 133, 133);
+    			gc9.setStroke(Color.DEEPPINK);
+    			gc9.setLineWidth(4);
+    			gc9.strokeLine(0, 0, 133, 133);
+    			win("O");
+    		} 
+    	}
+    	if(clickThree && clickFive && clickSeven) {
+    		if(threeXO.equals("x") && fiveXO.equals("x") && sevenXO.equals("x")) {
+    			System.out.println("X WIN");
+    			gc3.setStroke(Color.DEEPPINK);
+    			gc3.setLineWidth(4);
+    			gc3.strokeLine(133, 0, 0, 133);
+    			gc5.setStroke(Color.DEEPPINK);
+    			gc5.setLineWidth(4);
+    			gc5.strokeLine(133, 0, 0, 133);
+    			gc7.setStroke(Color.DEEPPINK);
+    			gc7.setLineWidth(4);
+    			gc7.strokeLine(133, 0, 0, 133);
+    			win("X");
+    		} else if(threeXO.equals("o") && fiveXO.equals("o") && sevenXO.equals("o")) {
+    			System.out.println("O WIN");
+    			gc3.setStroke(Color.DEEPPINK);
+    			gc3.setLineWidth(4);
+    			gc3.strokeLine(133, 0, 0, 133);
+    			gc5.setStroke(Color.DEEPPINK);
+    			gc5.setLineWidth(4);
+    			gc5.strokeLine(133, 0, 0, 133);
+    			gc7.setStroke(Color.DEEPPINK);
+    			gc7.setLineWidth(4);
+    			gc7.strokeLine(133, 0, 0, 133);
+    			win("O");
+    		} 
+    	}
+    }
+    
+    public void win(String a) {
+    	winLabel.setText(a + " wins!");
+    	/*
+    	if(clickOne == false) {
+    		clickOne = true;
+    	}
+    	if(clickTwo == false) {
+    		clickTwo = true;
+    	}
+    	if(clickThree == false) {
+    		clickThree = true;
+    	}
+    	if(clickFour == false) {
+    		clickFour = true;
+    	}
+    	if(clickFive = false) {
+    		clickFive = true;
+    	}
+    	if(clickSix = false) {
+    		clickSix = true;
+    	}
+    	if(clickSeven = false) {
+    		clickSeven = true;
+    	}
+    	if(clickEight = false) {
+    		clickEight = true;
+    	}
+    	if(clickNine = false) {
+    		clickNine = true;
+    	} */
+    	
+    	/*clickOne = true;
+    	clickTwo = true;
+    	clickThree = true;
+    	clickFour = true;
+    	clickFive = true;
+    	clickSix = true;
+    	clickSeven = true;
+    	clickEight = true;
+    	clickNine = true;*/
+    }
+    
+    @FXML
+    void quit(ActionEvent event) {
+
+    	System.exit(0);
     }
 
 }
